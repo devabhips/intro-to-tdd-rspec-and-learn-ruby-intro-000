694f9d9b25fdabbefab3ff63c06834e2c467fde3
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-09 17:43:21 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
 
 def current_age_for_birth_year(birth_year)
   current_year = Time.now.year
   age = current_year - birth_year
 end
 
-
-print "What is your birth year? "
+print 'What is your birth year? '
 birth_year = gets.to_i
 
 age = current_age_for_birth_year(birth_year)
 
-puts "Your age is #{ age }"
+puts "Your age is #{age}"
